3bb308dfaefd4b652202bca8d79f2ffa1ae5eb63
M9china <nqobilencaza2@gmail.com>
2024-03-23 17:03:36 +0200

    updated ip addresses

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['100.26.154.255', '18.207.207.212']
 
 
 def do_pack():
